6da5b3d909a011585723a46c5f364b87ead768c6
Vinicius Oliveira <162450936+vinioliveirasb@users.noreply.github.com>
2026-03-18 14:52:16 -0300

    Create main.java

---
+++ MissionariosCanibais-desafio/main.java
@@ -0,0 +1,37 @@
+package MissionariosCanibais;
+
+import busca.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Estado inicial = new MissionariosCanibais(3,3,0);
+
+        BuscaLargura busca = new BuscaLargura();
+
+        Nodo solucao = busca.busca(inicial);
+
+        if (solucao != null) {
+
+            List<Estado> caminho = new ArrayList<>();
+
+            Nodo atual = solucao;
+
+            while (atual != null) {
+                caminho.add(atual.getEstado());
+                atual = atual.getPai();
+            }
+
+            Collections.reverse(caminho);
+
+            for (Estado e : caminho) {
+                System.out.println(e);
+            }
+
+        } else {
+            System.out.println("Sem solução");
+        }
+    }
+}
